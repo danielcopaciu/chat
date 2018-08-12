e4a07ee0afec9f801fda78215f50ec3269e98f48
Zoltan Bodor <zbodor@utilitywarehouse.co.uk>
2018-08-12 15:02:22 +0100

    Revert "Add hardcoded autocert"
    This reverts commit c39dd5ceac27d70028c63db1f8412c8633ef2e28.

--- grpc.go
+++ grpc.go
@@ -1,20 +1,19 @@
 package main
 
 import (
+	"net"
+
 	"github.com/danielcopaciu/chat/generated/chat"
-	"golang.org/x/crypto/acme/autocert"
 
 	"github.com/cloudflare/cfssl/log"
 	"google.golang.org/grpc"
 )
 
 func startGRPCServer(address string, server chat.ChatServer) (func(), error) {
-	// lis, err := net.Listen("tcp", address)
-	// if err != nil {
-	// 	return nil, err
-	// }
-
-	lis := autocert.NewListener("chat.dragffy.ro")
+	lis, err := net.Listen("tcp", address)
+	if err != nil {
+		return nil, err
+	}
 
 	grpcServer := grpc.NewServer()
 	chat.RegisterChatServer(grpcServer, server)
