c39dd5ceac27d70028c63db1f8412c8633ef2e28
Zoltan Bodor <zbodor@utilitywarehouse.co.uk>
2018-08-12 14:55:42 +0100

    Add hardcoded autocert

--- grpc.go
+++ grpc.go
@@ -1,19 +1,20 @@
 package main
 
 import (
-	"net"
-
 	"github.com/danielcopaciu/chat/generated/chat"
+	"golang.org/x/crypto/acme/autocert"
 
 	"github.com/cloudflare/cfssl/log"
 	"google.golang.org/grpc"
 )
 
 func startGRPCServer(address string, server chat.ChatServer) (func(), error) {
-	lis, err := net.Listen("tcp", address)
-	if err != nil {
-		return nil, err
-	}
+	// lis, err := net.Listen("tcp", address)
+	// if err != nil {
+	// 	return nil, err
+	// }
+
+	lis := autocert.NewListener("chat.dragffy.ro")
 
 	grpcServer := grpc.NewServer()
 	chat.RegisterChatServer(grpcServer, server)
